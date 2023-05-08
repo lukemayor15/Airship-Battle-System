5cae694297358ffb7dc5859634103003a2970c02
lukemayor15 <lukemayor15@hotmail.co.uk>
2023-05-08 16:05:52 +0100

    Update Action_Select_Delgate.cs

--- Assets/Scrpits/AirShip/Code/UI/Battle UI/Attack_Panel/Action_Select_Delgate.cs
+++ Assets/Scrpits/AirShip/Code/UI/Battle UI/Attack_Panel/Action_Select_Delgate.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Handles the logic when an action have been select, this is triggerd when an attacl button is pressed and enabled the selecting of targetable objects.
 public class ActionSelect
 {
 
